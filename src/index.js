f67225f24f1cb9a5416a37f6cb5ebba74c1938c2
SROR7 <shahendareda13@gamil.com>
2026-01-31 00:14:20 +0200

    update mongo Db

--- src/index.js
+++ src/index.js
@@ -21,34 +21,34 @@ redisClient.connect();
 
 
 
-// connect postgres-DB 
-const DB_USER = 'root';
-const DB_PASSWORD = 'example';
-const DB_PORT = 5432;
-const DB_HOST = 'PG-DB';
-
-const URI =`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:3211`
- 
-const client = new Client({
-    connectionString: URI,
-});
-client
-    .connect()
-    .then(() => console.log('connected to Postgres-DB'))
-    .catch((err) => console.log('faild to connected to Postgres-DB', err))
-
-
-// // connect DB 
+// // connect postgres-DB 
 // const DB_USER = 'root';
 // const DB_PASSWORD = 'example';
-// const DB_PORT = 27017;
-// const DB_HOST = 'mongo';
+// const DB_PORT = 5432;
+// const DB_HOST = 'PG-DB';
 
-// const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`
-// mongoose
-//     .connect(URI)
-//     .then(() => console.log('connect to db'))
-//     .catch((err) => console.log('faild to connected to DB', err))
+// const URI =`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:3211`
+ 
+// const client = new Client({
+//     connectionString: URI,
+// });
+// client
+//     .connect()
+//     .then(() => console.log('connected to Postgres-DB'))
+//     .catch((err) => console.log('faild to connected to Postgres-DB', err))
+
+
+// connect DB 
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 27017;
+const DB_HOST = 'mongo';
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`
+mongoose
+    .connect(URI)
+    .then(() => console.log('connect to db'))
+    .catch((err) => console.log('faild to connected to DB', err))
 
 
 app.get( '/', (req, res) => {
